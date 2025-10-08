8df367cb3d93981909142c5d4892250b3b4c51cd
Joan Amengual <jamengual150899@gmail.com>
2025-10-08 13:49:20 +0200

    vm

--- Azure Virtual Machines (VM)/create-vm.ps1
+++ Azure Virtual Machines (VM)/create-vm.ps1
@@ -1,59 +1,40 @@
-# create-vm.ps1
-
-# Configuration
-$adminUsername = "joanamengual"
-$adminPassword = ConvertTo-SecureString "SecurePassword!" -AsPlainText -Force
+$adminUser = "joanamengual"
+$adminPassword = ConvertTo-SecureString "SecurePassword2025!" -AsPlainText -Force
 $location = "westeurope"
 $resourceGroup = "MyResourceGroup"
 $computerName = "MyVM"
 $vmName = "MyVM"
 $vmSize = "Standard_B1s"
 
-# Network configuration
+# Ensure resource group exists
+if (-not (Get-AzResourceGroup -Name $resourceGroup -ErrorAction SilentlyContinue)) {
+    New-AzResourceGroup -Name $resourceGroup -Location $location
+}
+
 $virtualNetworkName = "MyVNet"
 $subnetName = "MySubnet"
 $nicName = "MyNIC"
-$subnetPrefix = "10.0.2.0/24"
+$subnetPrefix = "10.0.0.0/24"
 $vnetPrefix = "10.0.0.0/16"
 
-# Create subnet config
-$subnetConfig = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetPrefix
+$singleSubnet = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetPrefix
 
-# Create virtual network
 $vnet = New-AzVirtualNetwork -Name $virtualNetworkName `
   -ResourceGroupName $resourceGroup `
   -Location $location `
   -AddressPrefix $vnetPrefix `
-  -Subnet $subnetConfig
+  -Subnet $singleSubnet
 
-# Create network interface
 $nic = New-AzNetworkInterface -Name $nicName `
   -ResourceGroupName $resourceGroup `
   -Location $location `
   -SubnetId $vnet.Subnets[0].Id
 
-# Create credentials
-$credentials = New-Object System.Management.Automation.PSCredential ($adminUsername, $adminPassword)
+$cred = New-Object System.Management.Automation.PSCredential($adminUser, $adminPassword)
 
-# VM configuration
-$vmConfig = New-AzVMConfig -VMName $vmName -VMSize $vmSize
+$vm = New-AzVMConfig -VMName $vmName -VMSize $vmSize
+$vm = Set-AzVMOperatingSystem -VM $vm -Windows -ComputerName $computerName -Credential $cred -ProvisionVMAgent -EnableAutoUpdate
+$vm = Add-AzVMNetworkInterface -VM $vm -Id $nic.Id
+$vm = Set-AzVMSourceImage -VM $vm -PublisherName "MicrosoftWindowsServer" -Offer "WindowsServer" -Skus "2022-datacenter-azure-edition" -Version "latest"
 
-$vmConfig = Set-AzVMOperatingSystem -VM $vmConfig -Windows `
-  -ComputerName $computerName `
-  -Credential $credentials `
-  -ProvisionVMAgent -EnableAutoUpdate
-
-$vmConfig = Add-AzVMNetworkInterface -VM $vmConfig -Id $nic.Id
-
-$vmConfig = Set-AzVMSourceImage -VM $vmConfig `
-  -PublisherName "MicrosoftWindowsServer" `
-  -Offer "WindowsServer" `
-  -Skus "2022-datacenter-azure-edition-core" `
-  -Version "latest"
-
-# Create the virtual machine
-New-AzVM `
-  -ResourceGroupName $resourceGroup `
-  -Location $location `
-  -VM $vmConfig `
-  -Verbose
+New-AzVM -ResourceGroupName $resourceGroup -Location $location -VM $vm -Verbose
